b6cebdbef523885211acb33d658af4be0d6f5780
13um45 <christian.leumas@icloud.com>
2016-04-22 00:25:26 -0400

    moved all files to appropriate folders.

--- js/reveal.js
+++
@@ -1,4 +0,0 @@
-window.sr = ScrollReveal();
-sr.reveal('.reveal_seq', { duration: 1000 }, 100);
-sr.reveal('.reveal_seq_2', { duration: 1000 }, 100);
-sr.reveal('.reveal', { duration: 1000 });
--- js/s_magic.js
+++
@@ -1,8 +0,0 @@
-  // init controller
-  var controller = new ScrollMagic.Controller();
-
-  // build scene
-  var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
-          // trigger a velocity opaticy animation
-          .setVelocity("#foo", {opacity: 0}, {duration: 400})
-          .addTo(controller);
